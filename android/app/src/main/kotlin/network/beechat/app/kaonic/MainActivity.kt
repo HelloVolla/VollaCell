843f2b67ba42f0e93772d2a3e88a51bd1293751f
Daria Ostapenko <daria.ostapenko@embrox.com>
2025-03-15 17:18:54 +0200

    added androidAudio

--- android/app/src/main/kotlin/network/beechat/app/kaonic/MainActivity.kt
+++ android/app/src/main/kotlin/network/beechat/app/kaonic/MainActivity.kt
@@ -1,8 +1,14 @@
 package network.beechat.app.kaonic
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.annotation.Keep
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel
 import network.beechat.app.kaonic.Kaonic
 
@@ -13,11 +19,38 @@ class MainActivity : FlutterActivity() {
     private val CHANNEL = "com.example.kaonic/kaonic"
     private val rxBuffer = ByteArray(2048)
 
+    private val CHANNEL_EVENT = "com.example.kaonic/audioStream"
+    private var eventSink: EventChannel.EventSink? = null
+    private var androidAudio: AndroidAudio? = null
+
+    external fun initKaonicLib(context: Context)
+    external fun openKaonicDevice()
+    external fun closeKaonicDevice()
+
+    external fun kaonicTransmit(data: ByteArray): Int
+    external fun kaonicReceive(data: ByteArray, timeout: Int): Int
+
+    external fun kaonicConfigure(rfIndex: Int, freq: Int, channel: Int, spacing: Int): Int
+
+    external fun codec2Encode(input: ByteArray): ByteArray
+    external fun codec2Decode(input: ByteArray): ByteArray
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         kaonic = Kaonic(this)
         serial = AndroidSerial(this)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Request permission
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                1
+            )
+        }
 
 
         MethodChannel(
@@ -34,50 +67,83 @@ class MainActivity : FlutterActivity() {
                 }
 
                 "closeDevice" -> {
-//                    closeKaonicDevice();
-//                    closeSerial();
+                    closeKaonicDevice();
+                    closeSerial();
 
                     result.success(true)
                 }
 
+                "codecEncode" -> {
+                    result.success(codec2Encode(call.argument<ByteArray>("data")!!))
+                }
+
+                "codecDecode" -> {
+                    result.success(codec2Decode(call.argument<ByteArray>("data")!!))
+                }
+
                 "kaonicTransmit" -> {
                     val data = call.argument<ByteArray>("data")
-//                    val rc = kaonicTransmit(
-//                        data!!,
-//                        call.argument<Int>("rfIndex")!!,
-//                        call.argument<Int>("trxType")!!
-//                    )
-//                    result.success(rc)
+                    val rc = kaonicTransmit(
+                        data!!
+                    )
+                    result.success(rc)
                 }
 
                 "kaonicReceive" -> {
+                    val rc = kaonicReceive(
+                        this.rxBuffer,
+                        call.argument<Int>("timeout")!!
+                    )
 
-//                    val rc = kaonicReceive(
-//                        this.rxBuffer,
-//                        call.argument<Int>("rfIndex")!!,
-//                        call.argument<Int>("trxType")!!,
-//                        call.argument<Int>("timeout")!!
-//                    )
-//
-//                    val resultData: HashMap<String, Any> = HashMap()
-//                    resultData["count"] = rc
-//                    resultData["data"] = this.rxBuffer
-//                    result.success(resultData)
+                    val resultData: HashMap<String, Any> = HashMap()
+                    resultData["count"] = rc
+                    resultData["data"] = this.rxBuffer
+                    result.success(resultData)
                 }
 
                 "kaonicConfigure" -> {
 
-//                    val rc = kaonicConfigure(
-//                        call.argument<Int>("rfIndex")!!,
-//                        call.argument<Int>("freq")!!,
-//                        call.argument<Int>("channel")!!,
-//                        call.argument<Int>("spacing")!!
-//                    )
-//
-//                    result.success(rc)
+                    val rc = kaonicConfigure(
+                        call.argument<Int>("rfIndex")!!,
+                        call.argument<Int>("freq")!!,
+                        call.argument<Int>("channel")!!,
+                        call.argument<Int>("spacing")!!
+                    )
+
+                    result.success(rc)
+                }
+                "startAudio"->{
+                    androidAudio?.startPlaying()
+                    androidAudio?.startRecording()
+                    result.success(0)
+                }
+                "stopAudio"->{
+                    androidAudio?.stopRecording()
+                    androidAudio?.stopPlaying()
+                    result.success(0)
+                }
+                "feedPlayer"->{
+                    val data = call.argument<ByteArray>("data")
+                    androidAudio?.play(data,data?.size?:0)
+                    result.success(0)
                 }
             }
         }
+
+        androidAudio = AndroidAudio(context)
+        EventChannel(flutterEngine?.dartExecutor?.binaryMessenger, CHANNEL_EVENT)
+            .setStreamHandler(
+                object : EventChannel.StreamHandler {
+                    override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+                        androidAudio?.eventSink = events
+
+                    }
+
+                    override fun onCancel(arguments: Any?) {
+                        androidAudio?.eventSink = null
+                    }
+                }
+            )
         
     }
 
@@ -87,12 +153,12 @@ class MainActivity : FlutterActivity() {
 
     fun openSerial(deviceName: String): Boolean {
         val opened = serial!!.open(deviceName)
-//        openKaonicDevice()
+        openKaonicDevice()
         return opened
     }
 
     fun closeSerial() {
-//        closeKaonicDevice()
+        closeKaonicDevice()
         serial!!.close()
     }
 
